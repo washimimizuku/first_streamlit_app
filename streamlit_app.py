98dc10e36a96e0d13b7c50549cf69a510f41d8e1
Nuno Barreto <nbarreto@gmail.com>
2023-02-01 16:44:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruity_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruity_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  retunr fruityvice_normalized
+  return fruityvice_normalized
   
 
 # New section to display fruityvice api response
